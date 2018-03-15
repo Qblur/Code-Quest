eec61e376b6ca94391536937f0bcb0cbf2658da8
Quantum Blur <31781013+QuantumBlur@users.noreply.github.com>
2018-03-15 13:02:40 -0400

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -7,27 +7,29 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-print('choose one thanks')
-print('1.add')
-print('2.subtract')
-print('3.multiply')
-print('4.divide')
+print('Enter a number:')
+print('1. Add')
+print('2. Subtract')
+print('3. Multiply')
+print('4. Divide')
 
 operation = input('Choose an operation(1,2,3,4):')
 
-
-number1 = int(input('first number'))
-number2 = int(input('second number'))
-
 if operation == '1':
+    number1 = int(input('Enter the first number:'))
+    number2 = int(input('Enter the second number:'))
     print(number1, '+', number2, '=', add(number1, number2))
 elif operation == '2':
+    number1 = int(input('Enter the first number:'))
+    number2 = int(input('Enter the second number:'))
     print(number1, '-', number2, '=', subtract(number1, number2))
 elif operation == '3':
+    number1 = int(input('Enter the first number:'))
+    number2 = int(input('Enter the second number:'))
     print(number1, '*', number2, '=', multiply(number1, number2))
 elif operation == '4':
+    number1 = int(input('Enter the first number:'))
+    number2 = int(input('Enter the second number:'))
     print(number1, '/', number2, '=', divide(number1, number2))
-if operation != (1,2,3,4):
-    print('wronginput')
-    
-    
+else:
+    print('Error')
