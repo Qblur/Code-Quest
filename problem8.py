fe2bd30f70c62d3c13a0f5ca158ee888bce91f68
quantumblurinc <31797536+quantumblurinc@users.noreply.github.com>
2018-03-17 21:04:19 -0400

    Add files via upload

---
+++ problem8.py
@@ -0,0 +1,26 @@
+import random
+def genlower():
+    letters = 'abcdefghijlmnopqrstuvwxyz'
+    y = random.randint(0,25)
+    return letters[y]
+
+
+    
+    
+    
+
+    
+    
+    
+
+
+    
+    
+    
+    
+    
+
+   
+
+    
+        
