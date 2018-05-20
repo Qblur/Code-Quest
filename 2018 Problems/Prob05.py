2b2a081d209c8f33ac7a88a009c457b99796ed75
MaxBabka <39202268+MaxBabka@users.noreply.github.com>
2018-05-20 19:20:03 -0400

    Add files via upload

---
+++ 2018 Problems/Prob05.py
@@ -0,0 +1,16 @@
+with open("Prob05.in.txt") as x:
+	cases = int(x.readline().replace("\n", ""))
+	for i in range(0, cases):
+		num = int(x.readline().replace("\n", ""))
+		temp = num
+		count = 0
+		while True:
+			if temp == 1:
+				break
+			if temp % 2 == 0:
+				count = count+1
+				temp = int(temp/2)
+			elif temp % 2 == 1:
+				count = count+1
+				temp = int(temp*3+1)
+		print(str(num)+":"+str(count))
