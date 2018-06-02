5406d9d12d640f8fa04abfa66cb1ca5f64f934e3
MaxBabka <39202268+MaxBabka@users.noreply.github.com>
2018-06-02 16:35:22 -0400

    Update Prob05.py

--- 2018 Problems/Prob05.py
+++ 2018 Problems/Prob05.py
@@ -1,16 +1,15 @@
-with open("Prob05.in.txt") as x:
-	cases = int(x.readline().replace("\n", ""))
-	for i in range(0, cases):
-		num = int(x.readline().replace("\n", ""))
-		temp = num
-		count = 1
-		while True:
-			if temp == 1:
-				break
-			if temp % 2 == 0:
-				count = count+1
-				temp = int(temp/2)
-			elif temp % 2 == 1:
-				count = count+1
-				temp = int(temp*3+1)
-		print(str(num)+":"+str(count))
+with open("Prob05.in.txt", "rt") as x:
+    cases = int(x.readline())
+    print(cases)
+    for i in range(0, cases):
+        current = int(x.readline())
+        count = 1
+        num = current
+        while num != 1:
+            if num % 2 == 0:
+                count += 1
+                num = num // 2
+            elif num % 2 == 1:
+                count += 1
+                num = (num*3)+1
+        print(str(current)+":"+str(count))
